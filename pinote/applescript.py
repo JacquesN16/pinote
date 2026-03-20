11511a4694aa1f1abfddac8d51ad0172888772bf
Jacques Nguyen <jacques@cadeaudelamaison.com>
2026-03-20 20:51:54 +0100

    fix list

--- pinote/applescript.py
+++ pinote/applescript.py
@@ -4,11 +4,12 @@ import subprocess
 GET_ALL_NOTES_SCRIPT = """
 tell application "Notes"
     set output to "["
-    set allNotes to every note 
+    set allNotes to every note
     repeat with i from 1 to count of allNotes
         set n to item i of allNotes
         set noteId to id of n
         set noteTitle to name of n
+          
         set output to output & "{\\"id\\":\\"" & noteId & "\\",\\"title\\":\\"" & noteTitle & "\\"}"
         if i < count of allNotes then set output to output & ","
     end repeat
@@ -27,6 +28,11 @@ def get_all_notes() -> list[dict]:
     return json.loads(result.stdout)
 
 
-def get_note_by_id(id: str) -> dict: ...  # fetch one notes
+def get_note_by_id(id: str) -> dict:
+    # fetch one notes
+    script = f"""
+tell application "Notes"
+    set n to 
+"""
 def create_note(title: str, body: str) -> dict: ...  # create new notes
 def update_note(id: str, title: str, body: str) -> None: ...  # update existing note
